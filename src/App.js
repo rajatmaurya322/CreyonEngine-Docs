bce2bf0a0c2e5c580adbb6b0e2aae829cfcf810f
blades <rajatmaurya1505@gmail.com>
2024-08-03 16:09:48 +0530

    Create navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SideBar from "./SideBar";
+import hamburger from "./images/hamburger.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="flex-row">
+        <SideBar/>
+        <nav className="flex-row items-center mt-05">
+          <div className="hamburger-icon">
+            <img src={hamburger} alt="hamburger.png"></img>
+          </div>
+          <div className="flex-around">
+            <div className="navigator">
+              <button>Previous</button>
+            </div>
+            <div className="selector">
+              <select>
+                <option>Asfsdfsdfsdf</option>
+                <option>A</option>
+                <option>Asfsfsdfsdfsdfsdfsdf</option>
+              </select>
+            </div>
+            <div className="navigator">
+              <button>Next</button>
+            </div>
+          </div>
+        </nav>
       </header>
     </div>
   );
