9f6cd3c3c8a1507b34900fd8c11198154ccd44e5
blades <rajatmaurya1505@gmail.com>
2024-08-04 19:08:03 +0530

    add style to navbar, adjust navigation for mobile

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,9 +1,14 @@
 import './SideBar.css'
-
+import hamburger from './images/hamburger.png'
 function SideBar(){
     return(
         <aside>
-            
+            <div>
+            <div className='hero'>
+                <img src={hamburger} alt='hero.png'></img>
+            </div> 
+            <div className='mt-1 hero-text'>CreyonEngine</div>
+            </div>
         </aside>
     );
 }
