7951e0720abfb25e066e29dbde00b3c8cf5d50ba
blades <rajatmaurya1505@gmail.com>
2024-08-09 17:23:17 +0530

    add chapter drive to sidebar

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,14 +1,10 @@
+import ChapterDrive from './ChapterDrive';
 import './SideBar.css'
-import hamburger from './images/hamburger.png'
+
 function SideBar(){
     return(
         <aside>
-            <div>
-            <div className='hero'>
-                <img src={hamburger} alt='hero.png'></img>
-            </div> 
-            <div className='mt-1 hero-text'>CreyonEngine</div>
-            </div>
+            <ChapterDrive/>
         </aside>
     );
 }
